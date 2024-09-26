248ed6394d1aa01c3d1226c92145bd69cb2d5af9
luna <lunamoon.ja@gmail.com>
2024-09-26 16:45:18 +0900

    add .env files & proxy env

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async redirects() {
-        return [
+  async redirects() {
+      return [
+        {
+          source: '/',
+          destination: '/bookmarks',
+          permanent: true
+        }
+      ]
+    }, // redirects
+
+    async rewrites() {
+      const API_BASE_URL = process.env.NEXT_PUBLIC_SERVER_API_BASE_URL
+      return [
           {
-            source: '/',
-            destination: '/bookmarks',
-            permanent: true
+              source: '/api/:path*',
+              destination: `${API_BASE_URL}/api/:path*`
+              //'http://bookmarker-api-svc:8080/:path*/' // Proxy to Backend
           }
-        ]
-      }
+      ]
+    }, //rewrites  
 };
 
 export default nextConfig;
--- src/app/components/Bookmarks.tsx
+++ src/app/components/Bookmarks.tsx
@@ -5,15 +5,14 @@ import Pagination from '@/app/components/Pagination';
 
 type Props = {
     bookmarks: BookmarksResponse
+    query?: string
 }
 
-const Bookmarks: React.FC<Props> = ({bookmarks}) => {
+const Bookmarks: React.FC<Props> = ({bookmarks, query}) => {
     return (
         <div>
-            <Pagination bookmarks={bookmarks} />
-            {
-            bookmarks.data.map(bookmark => <Bookmark key={bookmark.id} bookmark={bookmark} />)
-            }            
+            <Pagination bookmarks={bookmarks} query={query} />
+            { bookmarks.data.map(bookmark => <Bookmark key={bookmark.id} bookmark={bookmark} />) }
         </div>
     );
 };
