fd63cfb2b4f63e02739fd6d26df828a26702446d
luna <lunamoon.ja@gmail.com>
2024-09-26 11:46:01 +0900

    layout.tsx update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,7 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import NavBar from "@/app/components/NavBar";
-
 export default function Home() {
   return (
     <div>
-      <NavBar />
-      <main>
         <h1>Welcome to Bookmarker</h1>
-      </main>
     </div>
   );
 }
